6d2ed5132c242a96693c2f6f6d5e424e26e4d936
Hembrent <143487953+Hembrent@users.noreply.github.com>
2025-01-27 14:28:45 +0100

    Initial commmit, interface and model added.

--- Models/AttractionModel.cs
+++ Models/AttractionModel.cs
@@ -5,9 +5,11 @@ namespace Models;
 public class AttractionModel: IAttractionModel
 {
     public virtual Guid AttractionId { get; set; }
-    public virtual string Name {  get; set; }
+    public virtual string Name { get; set; }
+    public virtual string Description { get; set; }
     public AttractionCategory Category {  get; set; }
     public IComment Comments { get; set; }
+    public IAddress Address { get; set; }
 
     public bool Seeded { get; set; }
 }
